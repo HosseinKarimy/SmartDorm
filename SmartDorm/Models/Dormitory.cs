164c4fefdaacf31660ff74d9eddd1a1979097f22
hossein karimy <hosseinkm01@gmail.com>
2025-11-26 21:17:37 +0330

    Add Dormity and Room models

---
+++ SmartDorm/Models/Dormitory.cs
@@ -0,0 +1,29 @@
+﻿namespace SmartDorm.Models;
+
+using System.ComponentModel.DataAnnotations;
+using SmartDorm.Enums;
+
+
+public class Dormitory
+{
+    public int DormitoryId { get; set; }
+
+    [MaxLength(100)]
+    public required string Name { get; set; }
+
+    [MaxLength(200)]
+    public string? Address { get; set; }
+
+    [MaxLength(100)]
+    public string? CampusLocation { get; set; }
+
+    public int TotalCapacity { get; set; }
+
+    public GenderType GenderType { get; set; }
+
+    // Not strictly needed, but helpful if you want "primary manager"
+    public int? ManagerId { get; set; }
+    public DormitoryManager? Manager { get; set; }
+
+    public ICollection<Room> Rooms { get; set; } = [];
+}
---
+++ SmartDorm/Models/Room.cs
@@ -0,0 +1,20 @@
+﻿namespace SmartDorm.Models;
+
+using SmartDorm.Enums;
+
+public class Room
+{
+    public int RoomId { get; set; }
+    public int DormitoryId { get; set; }
+    public Dormitory Dormitory { get; set; } = null!;
+
+    public int RoomNumber { get; set; }
+    public int Floor { get; set; }
+    public int WingOrBlock { get; set; }
+    public int Capacity { get; set; }
+
+    public bool HasPrivateBathroom { get; set; }
+    public GenderType GenderType { get; set; }
+
+    public bool IsActive { get; set; } = true;
+}
